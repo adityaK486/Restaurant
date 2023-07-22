21f7403db821bfa6478d333c3c43b2659f3bf59f
Aditya Kumar <aditya201292@gmail.com>
2023-07-22 12:26:06 +0530

    Add eventListener to options

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import './home.js';
+import home from './home.js';
+import  menu from './menu.js'
+
+const content = document.getElementById('content');
+const homebtn = document.querySelector('.homebtn');
+const menubtn = document.querySelector('.menubtn');
+const contactbtn = document.querySelector('.contactbtn');
+
+content.appendChild(home);
+
+homebtn.addEventListener('click',()=>{
+    content.removeChild(menu);
+    content.appendChild(home);
+});
+
+
+menubtn.addEventListener('click',()=>{
+    content.removeChild(home);
+    content.appendChild(menu);
+});
